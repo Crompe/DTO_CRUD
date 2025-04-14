2197d91222f3e7c7e4dbbb37270c81cd30b9558c
Crompe <marcelalosada015@gmail.com>
2025-04-14 15:00:40 -0500

    Agregué los primeros archivos

---
+++ src/main/java/com/example/Proyecto_DTO_CRUD/ProyectoDtoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_DTO_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDtoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDtoCrudApplication.class, args);
+	}
+
+}
